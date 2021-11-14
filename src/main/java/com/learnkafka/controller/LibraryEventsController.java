cf7398074c63fcd3f5e6478cbf49bc14ce36e963
Junaid KHALID <junaidkhalid00007@gmail.com>
2021-11-14 01:29:07 +0100

    Adding Validations to the LibraryEvent Payload

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -24,7 +25,7 @@ public class LibraryEventsController {
     LibraryEventProducer producer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
         libraryEvent.setLibraryEventType(LibraryEventType.NEW);
         producer.sendLibraryEvent_Approach3(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
