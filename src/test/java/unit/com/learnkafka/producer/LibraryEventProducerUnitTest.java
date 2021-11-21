ea1698bb8930ed03417f4e3b01040bb8ee1e1db8
Junaid KHALID <junaidkhalid00007@gmail.com>
2021-11-21 03:38:16 +0100

    Write JUnit Test for Kafka Producer - OnSuccess()

--- src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
+++ src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +15,7 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.SettableListenableFuture;
 
 import java.util.concurrent.ExecutionException;
@@ -54,5 +57,38 @@ public class LibraryEventProducerUnitTest {
 
         //expect
         assertThrows(Exception.class, () -> producer.sendLibraryEvent_Approach3(libraryEvent).get());
+    }
+
+    @Test
+    void sendLibraryEvent_Approach3_success() throws JsonProcessingException, ExecutionException, InterruptedException {
+        //given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Junaid")
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String record = objectMapper.writeValueAsString(libraryEvent);
+
+        SettableListenableFuture<SendResult<Integer,String>> future = new SettableListenableFuture<>();
+        ProducerRecord<Integer,String> producerRecord = new ProducerRecord<>("library-events",libraryEvent.getLibraryEventId(),record);
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("library-events", 1),1,1,342,System.currentTimeMillis(),1,2);
+        SendResult<Integer,String> sendResult = new SendResult<>(producerRecord,recordMetadata);
+
+        future.set(sendResult);
+
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
+
+        //when
+        ListenableFuture<SendResult<Integer,String>> listenableFuture = producer.sendLibraryEvent_Approach3(libraryEvent);
+
+        //then
+        SendResult<Integer,String> sendResult1 = listenableFuture.get();
+        assert sendResult1.getRecordMetadata().partition() == 1;
     }
 }
